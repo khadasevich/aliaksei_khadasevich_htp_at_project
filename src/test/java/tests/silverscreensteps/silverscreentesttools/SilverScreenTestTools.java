d16832a89d45f4519d65cc65c53b51204c04ff38
AlexeyKhodasevich <khodasevich.aleksey@gmail.com>
2020-06-04 00:13:41 +0300

    First test refactored

--- src/test/java/tests/silverscreensteps/silverscreentesttools/SilverScreenTestTools.java
+++ src/test/java/tests/silverscreensteps/silverscreentesttools/SilverScreenTestTools.java
@@ -17,8 +17,8 @@ public class SilverScreenTestTools {
             flag = 1;
         }
         for (SilverScreenSearchResult searchResult : searchResults) {
-            if (!searchResult.getSearchName().contains(name) &&
-                    !searchResult.getSearchDescription().contains(name)) {
+            if (!searchResult.getSearchName().contains(name.toLowerCase()) &&
+                    !searchResult.getSearchDescription().contains(name.toLowerCase())) {
                 System.out.println("Following search result doesn't contain: " + name);
                 System.out.println("Name: " + searchResult.getSearchName() + "\n");
                 System.out.println("Description: " + searchResult.getSearchDescription() + "\n");
